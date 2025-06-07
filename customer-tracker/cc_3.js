eb8ba653b8b81a846acb9b1db403a8c75104adac
Zach-calvac <calvaccaz@usf.edu>
2025-06-07 13:16:47 -0400

    initial commit

---
+++ customer-tracker/cc_3.js
@@ -0,0 +1,24 @@
+let customers = [
+  {
+    name: "Joe Momma",
+    email: "Joe.Momma@gmail.com",
+    purchases: ["Computer", "Mouse", "Keyboard"]
+  },
+  {
+    name: "Patrick Mahomes",
+    email: "Pmahomie@gmail.com",
+    purchases: ["Iphone", "AirPods"]
+  },
+  {
+    name: "Charlie Brown",
+    email: "charlieB@gmail.com",
+    purchases: ["Tablet", "Pen", "Case"]
+  }
+];
+customers.push({
+  name: "Dana White",
+  email: "danaUFC@gmail.com",
+  purchases: ["Microphone"]
+});
+
+console.log(customers);
