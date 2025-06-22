a65319871e8014ca077d02466c4b55880520412c
Zach-calvac <calvaccaz@usf.edu>
2025-06-21 21:29:55 -0400

    Added an array of 4 employees

---
+++ Payroll/cc_5.js
@@ -0,0 +1,24 @@
+const employees = [
+  {
+    name: "Bryson DeChambeau",
+    hourlyRate: 25,
+    hoursWorked: 40
+  },
+  {
+    name: "Tiger Woods",
+    hourlyRate: 28,
+    hoursWorked: 36
+  },
+  {
+    name: "John Daly",
+    hourlyRate: 30,
+    hoursWorked: 40
+  },
+  {
+    name: "Sahith Theegala",
+    hourlyRate: 16,
+    hoursWorked: 47
+  }
+];
+
+console.log(employees);
