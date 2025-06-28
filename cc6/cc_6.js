a8d137d29b51868bb021eec01f6c937a8e18e206
Zach-calvac <calvaccaz@usf.edu>
2025-06-28 12:54:41 -0400

    redefined class and manager

--- cc6/cc_6.js
+++ cc6/cc_6.js
@@ -82,3 +82,41 @@ class Company {
     });
   }
 }
+
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  describe() {
+    return `${this.name} works in the ${this.department} department.`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${super.describe()} They manage a team of ${this.teamSize} people.`;
+  }
+}
+
+class Company {
+  constructor() {
+    this.employees = [];
+  }
+
+  addEmployee(employee) {
+    this.employees.push(employee);
+  }
+
+  listEmployees() {
+    this.employees.forEach(emp => {
+      console.log(emp.describe());
+    });
+  }
+}
