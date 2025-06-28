53f2e608dbc755d675c6e5e856deb4e1b6e6ff73
Zach-calvac <calvaccaz@usf.edu>
2025-06-28 12:30:02 -0400

    added manager subclass

--- cc6/cc_6.js
+++ cc6/cc_6.js
@@ -9,3 +9,24 @@ class Employee {
   }
 }
 
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  describe() {
+    return `${this.name} works in the ${this.department} department.`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department); 
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${super.describe()} They manage a team of ${this.teamSize} people.`;
+  }
+}
