08fbc3740af0a112f3576ff6f5754d63be21ea2a
Zach-calvac <calvaccaz@usf.edu>
2025-06-28 12:36:04 -0400

    added sample employees and managers

--- cc6/cc_6.js
+++ cc6/cc_6.js
@@ -30,3 +30,39 @@ class Manager extends Employee {
     return `${super.describe()} They manage a team of ${this.teamSize} people.`;
   }
 }
+ 
+class Employee {
+  constructor(name, department) {
+    this.name = name;
+    this.department = department;
+  }
+
+  describe() {
+    return `${this.name} works in the ${this.department} department.`;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, department, teamSize) {
+    super(name, department);
+    this.teamSize = teamSize;
+  }
+
+  describe() {
+    return `${super.describe()} They manage a team of ${this.teamSize} people.`;
+  }
+}
+
+const emp1 = new Employee("Lightning McQueen", "Marketing");
+const emp2 = new Employee("Chick Hicks", "Sales");
+const emp3 = new Employee("Doc Hudson", "Human Resources");
+
+const mgr1 = new Manager("Emilio.", "Engineering");
+const mgr2 = new Manager("Luigi.", "Finance");
+
+console.log(emp1.describe());
+console.log(emp2.describe());
+console.log(emp3.describe());
+
+console.log(mgr1.describe());
+console.log(mgr2.describe());
